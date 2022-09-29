252d6c9ff9633ae5a6f7c21805a4b9246e67e8e7
Alexsander <alexsander1996@yahoo.com.br>
2022-09-29 16:11:47 -0300

    Initial commit
    Project base settings

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Alex Landing Page</title>
+        <meta name="description" content="One page with all social links" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main></main>
+
+      <footer></footer>
+    </div>
+  );
+}
